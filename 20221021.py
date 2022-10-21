10d81b23cad57f504aedc05929b256cd7d442d0a
ohoCoding <youngho7482@gmail.com>
2022-10-21 11:52:48 +0900

    20221021_study

---
+++ 20221021.py
@@ -0,0 +1,36 @@
+# 정수형
+a = 123
+print(a)
+
+# 실수형
+b = 1.2
+print(b)
+
+# 사칙연산
+c = 5
+d = 4
+print(c + d)
+print(c * d)
+print(c / d)
+print(c**d)
+print(c % d)
+print(c // d)
+
+# 문자열 자료형
+##1. 큰따옴표로("")로 양쪽 둘러싸기
+print("Hello World")
+##2. 작은따옴표('')로 양쪽 둘러싸기
+print("Python is fun")
+##3. 큰따옴표 3개를 연속(""")으로 써서 양쪽 둘러싸기
+print("""Life is too short, You need python""")
+##4. 작은 따옴표 3개를 연속(''')으로 써서 양쪽 둘러싸기
+print("""Life is too short, You need python""")
+
+# 문자열 안에 작은 따옴표나 큰따옴표를 포함시킬때
+food = "Python's favorite food is perl"
+print(food)
+# 문자열 안에 큰 따옴표 포함
+say = '"Python is very easy." he says'
+print(say)
+# 여러줄인 문자열을 변수에 대입하고 싶을떄 
+
